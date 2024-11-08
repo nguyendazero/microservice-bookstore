80a2f6bb595ea988c9d79bb8da7ddc12e83e02ae
Nguyen Da Da <nguyendazero@gmail.com>
2024-11-08 11:53:16 +0700

    completed loadBalancer

--- api-gateway/src/main/java/com/bookstore/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bookstore/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,23 @@ package com.bookstore.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
